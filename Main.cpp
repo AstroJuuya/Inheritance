0882144a4840eda849128bcc591efe9626ed6a30
Juuya <AstroJuuya@protonmail.com>
2023-06-30 19:02:16 +0200

    added MemeFighter and it's children

--- Main.cpp
+++ Main.cpp
@@ -21,6 +21,128 @@ private:
 	std::mt19937 rng = std::mt19937( std::random_device{}() );
 };
 
+class MemeFighter
+{
+public:
+    virtual const std::string GetName() const { return m_name; }
+    int GetHealth() const { return m_hp; }
+    int GetInitiative() { return m_speed + m_dice.Roll(2); } // speed + 2d6
+    bool IsAlive() const { return m_hp > 0; }
+
+    void Punch(MemeFighter& other) {
+        //  damage opponent by power + 2d6
+        if (other.IsAlive() && IsAlive()) {
+            int damage = m_power + m_dice.Roll(2);
+            std::cout << GetName() << " punches " << other.GetName() << '.' << std::endl;
+            other.Damage(damage);
+        }
+    }
+
+    virtual void Tick() {
+        // recover 1d6 hp every turn
+        if (IsAlive()) {
+        int amount = m_dice.Roll(1);
+        std::cout << GetName() << " recovers " << amount << " HP." << std::endl;
+        Heal(amount);
+        }
+    }
+
+    virtual void Damage(int amount) {
+        m_hp -= amount;
+        std::cout << GetName() << " loses " << amount << " HP." << std::endl;
+        if (!IsAlive()) {
+            std::cout << GetName() << " stumbles and keels over dead." << std::endl;
+        }
+    }
+    void Heal(int amount) { m_hp += amount; }
+
+protected:
+    MemeFighter(std::string name, int hp, int speed, int power)
+        :
+            m_name(name),
+            m_hp(hp),
+            m_speed(speed),
+            m_power(power)
+    {}
+
+protected:
+    Dice m_dice;
+    std::string m_name = "Empty";
+    int m_hp = 1;
+    int m_speed = 1;
+    int m_power = 1;
+};
+
+class MemeFrog : public MemeFighter
+{
+public:
+    MemeFrog(std::string name)
+        :
+            MemeFighter(name, 69, 7, 14)
+    {}
+
+    void SpecialMove(MemeFighter& other) {
+        // 1/3 chance to deal 3d6 + 20 damage
+        if(other.IsAlive()) {
+            if(m_dice.Roll(1) <= 2) {
+                int damage = m_dice.Roll(3) + 20;
+                std::cout << GetName() << " roundhouse kicks " << other.GetName() << std::endl;
+                other.Damage(damage);
+            }
+            else {
+                std::cout << GetName() << " falls off the unicycle." << std::endl;
+            }
+        }
+    }
+
+    void Tick() override {
+        // damage by 1d6 every turn
+        std::cout << GetName() << " was hurt by the bad AIDS." << std::endl;
+        Damage(m_dice.Roll(1));
+    }
+};
+
+class MemeStoner : public MemeFighter
+{
+public:
+    MemeStoner(std::string name)
+    :
+        MemeFighter(name, 80, 4, 10)
+    {};
+
+    const std::string GetName() const override{
+        if (is_super) {
+            return "Super " + MemeFighter::GetName();
+        }
+        return MemeFighter::GetName();
+    }
+
+    void SpecialMove() {
+        // 1/2 chance to power up and increase health, speed, power
+        // and add "Super" to their name
+        if (IsAlive()) {
+            if (is_super) {
+                std::cout << MemeFighter::GetName() << " smokes another fat one " << std::endl;
+                return;
+            }
+            if (m_dice.Roll(1) <= 3) {
+                m_hp += 10;
+                m_speed += 3;
+                m_power += (int)(69 / 42);
+                is_super = true;
+                std::cout << MemeFighter::GetName()
+                    << " smokes a fat one and becomes " << GetName() << "!" << std::endl;
+            }
+            else {
+                std::cout << GetName() << "'s lighter isn't working." << std::endl;
+            }
+        }
+    }
+
+private:
+    bool is_super = false;
+};
+
 void Engage( MemeFighter& f1,MemeFighter& f2 )
 {
 	// pointers for sorting purposes
--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ PROJECT = Inheritance
 # CXX = g++
 CC = zig cc
 CXX = zig c++
-CXXFLAGS_COMMON = -std=gnu++2b -Werror -Wall -Wextra -fsanitize=undefined,address
+CXXFLAGS_COMMON = -std=gnu++2b -Wall -Wextra
 CXXFLAGS_RELEASE = -O2
 CXXFLAGS_DEBUG = -g -O0
 LDFLAGS = -Bstatic
@@ -11,15 +11,18 @@ LIBS =
 
 BUILDDIR = build/
 HEADER_FILES = 
-SOURCE_FILES = Main
+SOURCE_FILES = Main.cpp
 
-$(SOURCE_FILES): CXXFLAGS = $(CXXFLAGS_COMMON) $(CXXFLAGS_DEBUG)
-$(BUILDDIR)Main: Main.cpp
+debug:  CXXFLAGS = $(CXXFLAGS_COMMON) $(CXXFLAGS_DEBUG)
+
+debug: $(SOURCE_FILES)
 	mkdir -p $(BUILDDIR)
 	$(CXX) $(CXXFLAGS) $(LDFLAGS) $^ -o $(BUILDDIR)$(PROJECT) $(LIBS)
 
 test:
 	$(BUILDDIR)$(PROJECT)
+
+run: debug test	
 
 clean:
 	rm -rf $(BUILDDIR)*
